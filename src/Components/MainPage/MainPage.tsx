f081d767aae980eff78810551ef7f976ded51245
Steven Zabala <zabalasteven9@gmail.com>
2025-07-04 11:45:49 -0500

    creating main page with img

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -4,7 +4,17 @@ import './MainPage.css';
 function MainPage() {
 
     return (
-        <label>test</label>
+        <div className='container-main-page'>
+            <label id='label-introduction'>Descubre nuestra ultima colección</label>
+            <button id='button-explorar'>EXPLORAR AHORA</button>
+
+            <div className='container-imagenes-main-page'>
+                <img src='https://relojes-s-l.s3.us-west-1.amazonaws.com/Richard+mille.JPG' alt='' ></img>
+                <img src='https://relojes-s-l.s3.us-west-1.amazonaws.com/Rolex+submarine+4.JPG' alt=''></img>
+                <img src='https://relojes-s-l.s3.us-west-1.amazonaws.com/Rolex+daytona.JPG' alt=''></img>
+                <img src='https://relojes-s-l.s3.us-west-1.amazonaws.com/Casio+EDIFICE+5.JPG' alt='' ></img>
+            </div>
+        </div>
     )
 };
 
