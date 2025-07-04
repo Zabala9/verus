3709c152501e9574ebec1dbdad67bd5d212b1f1d
Steven Zabala <zabalasteven9@gmail.com>
2025-07-03 22:30:55 -0500

    calling browser router and adding routes design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './Components/NavBar/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+// imports all components necessary
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar></Navbar>
-    </>
+      <Routes>
+        {/* <Route path='/' element={} /> */}
+      </Routes>
+    </Router>
   );
 }
 
