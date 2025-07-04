7e83e22e79b36905ea78644b535b89bc4649cc5a
Steven Zabala <zabalasteven9@gmail.com>
2025-07-04 12:14:12 -0500

    adding footer and styling it

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -7,7 +7,8 @@ function MainPage() {
     return (
         <div className='container-main-page'>
             <label id='label-introduction'>Descubre nuestra ultima colección</label>
-            <Link to='/catalogo' id='button-explorar'>EXPLORAR AHORA</Link>
+            <Link to='/relojesz&l' id='button-explorar'>EXPLORAR AHORA</Link>
+            {/* en el futuro cambiar link para catalogo (ver todo) */}
 
             <div className='container-imagenes-main-page'>
                 <img src='https://relojes-s-l.s3.us-west-1.amazonaws.com/Richard+mille.JPG' alt='' ></img>
