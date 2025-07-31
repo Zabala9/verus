8e803929ff1d1ca331e12c6fd484307df37c6d9f
Steven Zabala <zabalasteven9@gmail.com>
2025-07-30 20:56:30 -0500

    creating Contact page

---
+++ src/Components/Contact/Contact.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Contact.css';
+
+function Contact() {
+
+    return (
+        <div className="container-contact">
+            <span id="label-canales">Nuestros canales oficiales: </span>
+
+            <a id="link-to-instagram" href="https://www.instagram.com/verus.2802/"
+                target="_blank" rel="noopener noreferrer" >
+                <img id="logo-instagram" src='/Assets/logo-instagram.jpg' alt="" ></img>
+                <span>INSTAGRAM</span>
+            </a>
+
+            <a id="link-to-whatsapp" href="https://wa.me/573132679419/"
+                target="_blank" rel="noopener noreferrer" >
+                <img id="logo-whatsapp" src='/Assets/logo-whatsapp.jpg' alt="" ></img>
+                <span>WHATSAPP</span>
+            </a>
+        </div>
+    )
+};
+
+export default Contact;
