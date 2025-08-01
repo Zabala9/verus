04833b92b2e9de8961d09b49858bd89da3d51f51
Steven Zabala <zabalasteven9@gmail.com>
2025-08-01 07:37:41 -0500

    updating browserRouter and HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './Components/NavBar/NavBar';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import MainPage from './Components/MainPage/MainPage';
@@ -8,19 +8,23 @@ import Relojes from './Components/Relojes/Relojes';
 import Contact from './Components/Contact/Contact';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
 import Ropa from './Components/Ropa/Ropa';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
+  const RouterComponent = process.env.NODE_ENV === 'production' ? HashRouter : BrowserRouter;
+
   return (
-    <Router>
+    <RouterComponent>
       <Navbar></Navbar>
       <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route path='/' element={<MainPage />} />
         <Route path='/relojes' element={<Relojes /> } />
         <Route path='/ropa' element={ <Ropa /> } />
         <Route path='/contacto' element={<Contact/>} />
         <Route path='/relojes/:id' element={<ProductDetail />} />
       </Routes>
-    </Router>
+    </RouterComponent>
   );
 }
 
