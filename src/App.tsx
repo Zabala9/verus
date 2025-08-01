b0f1d35d2351a570a7f29c7289f063a4d1710f33
Steven Zabala <zabalasteven9@gmail.com>
2025-07-31 20:30:50 -0500

    making show all watches into relojes page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import MainPage from './Components/MainPage/MainPage';
+import Relojes from './Components/Relojes/Relojes';
 import Contact from './Components/Contact/Contact';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        {/* <Route path='/relojes' element={ relojes aqui } /> */}
+        <Route path='/relojes' element={<Relojes /> } />
         {/* <Route path='/ropaz' element={ ropa aqui } /> */}
         <Route path='/contacto' element={<Contact/>} />
       </Routes>
