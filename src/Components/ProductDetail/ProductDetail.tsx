19f6124daabc17f762cd65e568306eba1a9a2b15
Steven Zabala <zabalasteven9@gmail.com>
2025-08-01 07:12:03 -0500

    adding button lo quiero and styling it

--- src/Components/ProductDetail/ProductDetail.tsx
+++ src/Components/ProductDetail/ProductDetail.tsx
@@ -50,6 +50,14 @@ function ProductDetail() {
             <label id="label-color"><strong>Color:</strong> {product.color}</label>
             <label>{product.presentacion}</label>
             <p><strong>Precio: </strong> ${product.price.toLocaleString()}</p>
+
+            <div className="container-contact-detail">
+                <a id="link-to-whatsapp" href="https://wa.me/573132679419/"
+                    target="_blank" rel="noopener noreferrer" >
+                    {/* <img id="logo-whatsapp" src={`${process.env.PUBLIC_URL}/Assets/logo-whatsapp.jpg`} alt="" ></img> */}
+                    <span id="label-quiero">LO QUIERO</span>
+                </a>
+            </div>
         </div>
     )
 };
