3f4f296c848b2cf589bc5af754a915de6fd87a71
Steven Zabala <zabalasteven9@gmail.com>
2025-08-01 07:11:20 -0500

    changing route for img whatsapp and instagram

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -9,13 +9,13 @@ function Contact() {
 
             <a id="link-to-instagram" href="https://www.instagram.com/verus.2802/"
                 target="_blank" rel="noopener noreferrer" >
-                <img id="logo-instagram" src='/Assets/logo-instagram.jpg' alt="" ></img>
+                <img id="logo-instagram" src={`${process.env.PUBLIC_URL}/Assets/logo-instagram.jpg`} alt="" ></img>
                 <span id="label-instragram">INSTAGRAM</span>
             </a>
 
             <a id="link-to-whatsapp" href="https://wa.me/573132679419/"
                 target="_blank" rel="noopener noreferrer" >
-                <img id="logo-whatsapp" src='/Assets/logo-whatsapp.jpg' alt="" ></img>
+                <img id="logo-whatsapp" src={`${process.env.PUBLIC_URL}/Assets/logo-whatsapp.jpg`} alt="" ></img>
                 <span id="label-whatsapp">WHATSAPP</span>
             </a>
         </div>
