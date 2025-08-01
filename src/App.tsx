271942bedff38b8a082a7e8d3f46f9a6676e78d8
Steven Zabala <zabalasteven9@gmail.com>
2025-08-01 00:04:35 -0500

    calling Ropa component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import MainPage from './Components/MainPage/MainPage';
 import Relojes from './Components/Relojes/Relojes';
 import Contact from './Components/Contact/Contact';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
+import Ropa from './Components/Ropa/Ropa';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/relojes' element={<Relojes /> } />
-        {/* <Route path='/ropaz' element={ ropa aqui } /> */}
+        <Route path='/ropa' element={ <Ropa /> } />
         <Route path='/contacto' element={<Contact/>} />
         <Route path='/relojes/:id' element={<ProductDetail />} />
       </Routes>
