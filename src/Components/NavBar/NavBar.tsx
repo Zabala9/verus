10a995df4f88bfd11b4169bb6f507d67217dd341
Steven Zabala <zabalasteven9@gmail.com>
2025-09-18 22:01:11 -0500

    holding filter brands when changing pages except on relojes

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -7,12 +7,20 @@ function Navbar() {
     <div className='nav-bar'>
       {/* <span id='label-test'>test</span> */}
       <div className='container-name'>
-        <Link to='/' id='label-name'>VERUS</Link>
+        <Link to='/' id='label-name'
+            // onClick={() => sessionStorage.removeItem('selectedBrands')}
+        >
+          VERUS
+        </Link>
       </div>
       <div className='container-opciones'>
         {/* <Link to='/' id='label-inicio'>INICIO</Link> */}
         <Link to='/relojes' id='label-relojes'>RELOJES</Link>
-        <Link to='/ropa' id='label-ropa'>ROPA</Link>
+        <Link to='/ropa' id='label-ropa'
+            onClick={() => sessionStorage.removeItem('selectedBrands')}
+        >
+          ROPA
+        </Link>
         <Link to='/contacto' id='label-contacto'>CONTACTO</Link>
       </div>
     </div>
