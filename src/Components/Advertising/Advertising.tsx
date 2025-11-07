53e3b305b799cd62588320ad41db256393802cf2
Steven Zabala <zabalasteven9@gmail.com>
2025-11-07 09:40:50 -0600

    creating advertising component

---
+++ src/Components/Advertising/Advertising.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Advertising.css';
+
+function Advertising() {
+    
+    return (
+        <div className="container-advertising">
+            <img id="advertising" src="https://relojes-s-l.s3.us-west-1.amazonaws.com/Black+Friday+2025+-+1.JPEG" alt=""></img>
+        </div>
+    )
+};
+
+export default Advertising;
