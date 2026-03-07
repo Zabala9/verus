b0d4b826d270787ad3e8a45780252a2609da280c
Steven Zabala <zabalasteven9@gmail.com>
2026-03-06 21:21:00 -0600

    adding air force 1 to Ropa

--- src/Components/Ropa/Ropa.tsx
+++ src/Components/Ropa/Ropa.tsx
@@ -5,8 +5,15 @@ import './Ropa.css';
 function Ropa() {
     return (
         <div className="container-ropa">
-            <p id="label-proximamente">PROXIMAMENTE...</p>
-            <Link to='/' id="button-go-back-main">VOLVER A LA PAGINA PRINCIPAL</Link>
+            <span id="title-container-ropa">Air Force 1</span>
+            <img id="image-container-ropa" src="https://relojes-s-l.s3.us-west-1.amazonaws.com/ROPA/Nike+AF1.jpeg" alt=""></img>
+
+            <a id="link-to-whatsapp-product-detail" href="https://wa.me/573132679419/"
+                target="_blank" rel="noopener noreferrer" >
+                {/* <img id="logo-whatsapp" src={`${process.env.PUBLIC_URL}/Assets/logo-whatsapp.jpg`} alt="" ></img> */}
+
+                <span id="label-quiero">LO QUIERO</span>
+            </a>
         </div>
     )
 };
