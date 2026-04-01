b811f2f6d84babe96faeb7524f0167c5e7474171
Steven Zabala <zabalasteven9@gmail.com>
2026-04-01 16:19:10 -0500

    creating Ropa types

---
+++ src/Types/Ropa.ts
@@ -0,0 +1,20 @@
+export const Ropa = [
+    {
+        "name": "Air Force 1",
+        "color": "Blanco",
+        "imgUrl": "https://relojes-s-l.s3.us-west-1.amazonaws.com/ROPA/Nike+AF1.jpeg",
+        "price": 140000
+    },
+    {
+        "name": "Air Force 1 x BAPE",
+        "color": "Blanco con detalles en verde",
+        "imgUrl": "https://relojes-s-l.s3.us-west-1.amazonaws.com/ROPA/Air+Force+1+x+Bape+verde.jpg",
+        "price": 150000
+    },
+    {
+        "name": "Air Force 1 x BAPE",
+        "color": "Blanco con detalles en azul",
+        "imgUrl": "https://relojes-s-l.s3.us-west-1.amazonaws.com/ROPA/Air+Force+1+x+Bape+azul.jpg",
+        "price": 150000
+    },
+]
