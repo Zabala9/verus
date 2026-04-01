2716459db296b9ca7ed4f92baf5183a1548678c2
Steven Zabala <zabalasteven9@gmail.com>
2026-04-01 15:45:01 -0500

    changing slider img

--- src/Components/Mayoristas/MayoristasSlider.tsx
+++ src/Components/Mayoristas/MayoristasSlider.tsx
@@ -3,7 +3,7 @@ import './MayoristasSlider.css';
 import { Link } from "react-router-dom";
 
 const bannerImages = [
-    'https://relojes-s-l.s3.us-west-1.amazonaws.com/MAYORISTAS/casio+retro+2.JPG',
+    'https://relojes-s-l.s3.us-west-1.amazonaws.com/MAYORISTAS/Hublot+4.JPG',
     'https://relojes-s-l.s3.us-west-1.amazonaws.com/MAYORISTAS/mulco+4.JPG',
     'https://relojes-s-l.s3.us-west-1.amazonaws.com/MAYORISTAS/orient+dama+8.JPG',
     'https://relojes-s-l.s3.us-west-1.amazonaws.com/MAYORISTAS/Q%26Q+fondo+lineas+4.JPG',
