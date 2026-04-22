ba058ea115de0c93d9e5c7d5c8e643c467694355
Steven Zabala <zabalasteven9@gmail.com>
2026-04-22 18:40:00 -0500

    adding routes for showing individual item in ropa

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ import MayoristasBrand from './Components/Mayoristas/MayoristasBrand';
 import Relojes from './Components/Relojes/Relojes';
 import Contact from './Components/Contact/Contact';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
-import Ropa from './Components/Ropa/Ropa';
+import RopaMain from './Components/Ropa/RopaMain';
+import RopaPage from './Components/Ropa/RopaPage';
+import ZapatosPage from './Components/Ropa/ZapatosPage';
 import NotFound from './Components/NotFound/NotFound';
 
 function App() {
@@ -21,9 +23,13 @@ function App() {
         <Route path='*' element={<NotFound />} />
         <Route path='/' element={<MainPage />} />
         <Route path='/relojes' element={<Relojes /> } />
-        <Route path='/ropa' element={ <Ropa /> } />
+        <Route path='/ropa' element={ <RopaMain /> } />
+        <Route path='/ropaverus' element={ <RopaPage /> } />
+        <Route path='/zapatosverus' element={ <ZapatosPage />} />
         <Route path='/contacto' element={<Contact/>} />
         <Route path='/relojes/:id' element={<ProductDetail />} />
+        <Route path='/zapatosverus/:id' element={<ProductDetail />} />
+        <Route path='/ropaverus/:id' element={<ProductDetail />} />
         <Route path='/verus-mayoristas' element={<Mayoristas />} />
         <Route path='/verus-mayoristas/:brand' element={<MayoristasBrand />} />
       </Routes>
